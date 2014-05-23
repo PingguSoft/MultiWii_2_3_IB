908f2f4708481148894c61d95f0e77191b916e86
PingguSoft <pinggusoft@gmail.com>
2014-05-23 09:24:50 +0900

    Initial upload

---
+++ EEPROM.h
@@ -0,0 +1,15 @@
+#ifndef EEPROM_H_
+#define EEPROM_H_
+
+void readGlobalSet();
+bool readEEPROM();
+void update_constants();
+void writeGlobalSet(uint8_t b);
+void writeParams(uint8_t b);
+void LoadDefaults();
+void readPLog(void);
+void writePLog(void);
+
+#endif /* EEPROM_H_ */
+
+
